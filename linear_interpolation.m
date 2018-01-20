91ef8152bb50dda079943578f537a3820a02c632
Kazi Mejbaul Islam <222mzb@gmail.com>
2018-01-20 21:38:48 +0600

    Update linear_interpolation.m

--- linear_interpolation.m
+++ linear_interpolation.m
@@ -1,6 +1,21 @@
+clear all
 clc
-clear all;
 
-x = [0:7];
-y = [.5 .34 .23 1.5 .23 6.3 2.6 .46];
+x = [0:6];
+y = [0 .8415 .9093 .1411 -.7568 -.9589 -.2794];
+plot(x,y,'o')
+n = length(x);
+x_i = input('x = ');
 
+for i = 1:n-1
+    if x_i>=x(i) && x_i<=x(i+1)
+        m = i;
+    end
+end
+
+xa = x(m)
+xb = x(m+1)
+ya = y(m)
+yb = y(m+1)
+
+y_k = (((x_i-xb)/(xa-xb))*ya) - (((x_i - xa)/(xa-xb))*yb);
